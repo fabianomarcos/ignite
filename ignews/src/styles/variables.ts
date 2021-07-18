0e71b184f318599b247c753fb1c268a8735ba111
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-07-18 11:11:54 -0300

    styles: global styles

---
+++ ignews/src/styles/variables.ts
@@ -0,0 +1,8 @@
+export const primary = "#0047BB";
+export const secondary = "#F06400";
+export const danger = "#D64B45";
+export const success = "#79C300";
+export const gray_001 = "#F5F8FA";
+export const gray_002 = "#657786";
+export const gray_003 = "#14171A";
+export const white = "#FFFFFF";
