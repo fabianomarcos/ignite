e0bcc8a9d0a30ab4e33220a719688803a9c476a4
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-07-22 22:55:25 -0300

    feat: added nextAuthProvider on project

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import {stripe} from "../services/stripe";
+
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+	product: {
+		priceId: string;
+		amount: number;
+	}
+}
+export default function Home({ product }: HomeProps) {
   return (
 	<>
 		<Head>
@@ -16,13 +25,32 @@ export default function Home() {
 				<h1>Noticias sobre o mundo <span>React</span>.</h1>
 				<p>
 					Tenha acesso a todas as publicações <br />
-					<span>por apenas $9,90 por mês.</span>
+					<span>por apenas {product.amount} por mês.</span>
 				</p>
-				<SubscribeButton />
+				<SubscribeButton priceId={product.priceId} />
 			</section>
 
 			<img src="/images/avatar.svg" alt="Girl coding" />
 		</main>
 	</>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	const price = await stripe.prices.retrieve("price_1JGB05IOrAeTw7UB14ofPA2e");
+
+	const product = {
+		priceId: price.id,
+		amount: new Intl.NumberFormat("pt-BR", {
+			style: "currency",
+			currency: "BRL",
+		}).format(price.unit_amount/100)
+	}
+
+	return {
+		props: {
+			product
+		},
+		revalidate: 60 * 60 * 24 // 24 hours
+	}
 }
