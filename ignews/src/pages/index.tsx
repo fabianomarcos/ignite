3ff5ec1423b76015313e77614c00d74a4d0ede89
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2022-07-09 15:23:31 -0300

    chore: fix api key

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -47,10 +47,10 @@ export const getStaticProps: GetStaticProps = async () => {
 		}).format(price.unit_amount/100)
 	}
 
+	const twentyFourHours = 60 * 60 * 24;
+
 	return {
-		props: {
-			product
-		},
-		revalidate: 60 * 60 * 24 // 24 hours
+		props: { product },
+		revalidate: twentyFourHours
 	}
 }
